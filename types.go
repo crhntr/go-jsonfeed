0c5cb6a4f5aacce07f1270728921b0aee21538f0
crhntr <crhntr.com@gmail.com>
2017-05-19 16:59:39 -0700

    added optional tags on optional atributes
    also all fields now have feeds that match spec

--- types.go
+++ types.go
@@ -5,21 +5,23 @@ import "time"
 type Feed struct {
 	Version     string `json:"version"`
 	Title       string `json:"title"`
-	HomeURL     string `json:"home_page_url"`
-	FeedURL     string `json:"feed_url"`
-	Description string `json:"description"`
-	UserComment string `json:"user_comment"`
-	NextURL     string `json:"next_url"`
-	Icon        string `json:"icon"`
-	Favicon     string `json:"favicon"`
-	Expired     bool   `json:"expired"`
-	Items       []Item `json:"items"`
+	HomeURL     string `json:"home_page_url,optional"`
+	FeedURL     string `json:"feed_url,optional"`
+	Description string `json:"description,optional"`
+	UserComment string `json:"user_comment,optional"`
+	NextURL     string `json:"next_url,optional"`
+	Icon        string `json:"icon,optional"`
+	Favicon     string `json:"favicon,optional"`
+	Expired     bool   `json:"expired,optional"`
+	Items       []Item `json:"items,optional"`
+
+	Hubs map[string]interface{} `json:"hubs,optional"`
 }
 
 type Author struct {
-	Name    string `json:"name"`
-	SiteURL string `json:"url"`
-	Avitar  string `json:"avitar"`
+	Name    string `json:"name,optional"`
+	SiteURL string `json:"url,optional"`
+	Avitar  string `json:"avitar,optional"`
 }
 
 type Hub struct {
@@ -28,25 +30,26 @@ type Hub struct {
 }
 
 type Item struct {
-	ID             string
-	URL            string
-	ExternalURL    string
-	Title          string
-	ContentHTML    string
-	Summary        string
-	ImageURL       string
-	BannerImageURL string
-	DatePublished  time.Time
-	DateModified   time.Time
-	Author         Author
-	Tags           []string
-	Attachments    []Attachment
+	ID             string       `json:"id"`
+	URL            string       `json:"url,optional"`
+	ExternalURL    string       `json:"external_url,optional"`
+	Title          string       `json:"title,optional"`
+	ContentHTML    string       `json:"content_html,optional"`
+	ContentText    string       `json:"content_text,optional"`
+	Summary        string       `json:"summary,optional"`
+	ImageURL       string       `json:"image,optional"`
+	BannerImageURL string       `json:"banner_image,optional"`
+	DatePublished  time.Time    `json:"date_published,optional"`
+	DateModified   time.Time    `json:"date_modified,optional"`
+	Author         Author       `json:"author,optional"`
+	Tags           []string     `json:"tags,optional"`
+	Attachments    []Attachment `json:"attachments,optional"`
 }
 
 type Attachment struct {
-	URL         string
-	MIMEType    string
-	Title       string
-	SizeInBytes int
-	Duration    time.Duration
+	URL         string        `json:"url"`
+	MIMEType    string        `json:"mime_type"`
+	Title       string        `json:"title,optional"`
+	SizeInBytes int           `json:"size_in_bytes,optional"`
+	Duration    time.Duration `json:"duration_in_seconds,optional"`
 }
